d1108845c776b21fc73587bf28d289ab3f8c34d5
RavinduThilina222 <COHNDSE233F-006@student.nibm.lk>
2024-12-25 12:31:34 +0530

    first commit in client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
 
 export default function App() {
-  return (
-    <div>
-      <h1>React App</h1>
-    </div>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/sign-in' element={<Signin/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+  </BrowserRouter>
 }
